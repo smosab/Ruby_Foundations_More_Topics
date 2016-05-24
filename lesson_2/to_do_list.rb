e78ac7f57d101271d2c7dba0cba1bdbedfdbd0e5
Mosab Sasi <smosab@gmail.com>
2016-05-23 21:13:13 -0600

    multiple changes, testing files

--- lesson_2/to_do_list.rb
+++ lesson_2/to_do_list.rb
@@ -165,19 +165,19 @@ class TodoList
   end
 end
 
-# todo1 = Todo.new("Buy milk")
-# todo2 = Todo.new("Clean room")
-# todo3 = Todo.new("Go to gym")
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
 
-# list = TodoList.new("Today's Todos")
-# list.add(todo1)
-# list.add(todo2)
-# list.add(todo3)
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
 
-# list.mark_all_done
-# puts list
+list.mark_all_done
+puts list
 
-# list.mark_all_undone
-# puts list
+list.mark_all_undone
+puts list
 
 
---
+++ lesson_2/todolist_test.rb
@@ -0,0 +1,50 @@
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require_relative 'to_do_list'
+
+class TodoListTest < MiniTest::Test
+
+  def setup
+    @todo1 = Todo.new("Buy milk")
+    @todo2 = Todo.new("Clean room")
+    @todo3 = Todo.new("Go to gym")
+    @todos = [@todo1, @todo2, @todo3]
+
+    @list = TodoList.new("Today's Todos")
+    @list.add(@todo1)
+    @list.add(@todo2)
+    @list.add(@todo3)
+  end
+
+  # Your tests go here. Remember they must start with "test_"
+
+  def test_to_a
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_size
+    assert_equal(3, @list.size)
+  end
+
+  def test_first
+    assert_equal(@todo1, @list.first)
+  end
+
+  def test_last
+    assert_equal(@todo3, @list.last)
+  end
+
+  def test_shift
+    assert_equal(@todo1, @list.shift)
+    assert_equal(@todo2, @list.shift)
+    assert_equal(@todo3, @list.shift)
+  end
+
+  def test_pop
+    todo = @list.pop
+    assert_equal(@todo3, todo)
+    assert_equal([@todo1, @todo2], @list.to_a)
+  end
+end

abca82f8fc6df33467b6ed53238ba44d12533cc3
Moe <smosab@gmail.com>
2016-05-26 03:54:20 +0000

    To do list assignments

--- lesson_2/todolist_test.rb
+++ lesson_2/todolist_test.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use!
@@ -10,7 +11,7 @@ class TodoListTest < MiniTest::Test
     @todo1 = Todo.new("Buy milk")
     @todo2 = Todo.new("Clean room")
     @todo3 = Todo.new("Go to gym")
-    @todo4 = Todo.new("Walk the dog")
+    
     @todos = [@todo1, @todo2, @todo3]
 
     @list = TodoList.new("Today's Todos")
@@ -66,5 +67,96 @@ class TodoListTest < MiniTest::Test
     todos = @todos << new_todo
 
     assert_equal(todos, @list.to_a)
+  end
+  
+  def test_item_at
+    # Hint: remember that this method should raise IndexError if we specify an index with no element.
+    assert_equal(@todo1, @list.item_at(0))
+    assert_equal(@todo2, @list.item_at(1))
+    assert_raises(IndexError) { @list.item_at(10) }
+  end
+  
+  def test_mark_done_at
+    assert_raises(IndexError) { @list.mark_done_at(100) }
+    @list.mark_done_at(0)
+    @list.mark_done_at(1)
+    assert_equal(true, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(false, @todo3.done?)
+  end
+  
+  def test_mark_undone_at
+    @list.mark_all_done
+    assert_raises(IndexError) { @list.mark_undone_at(100) }
+    @list.mark_undone_at(0)
+    @list.mark_undone_at(1)
+    assert_equal(false, @todo1.done?)
+    assert_equal(false, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+  end
+  
+  def test_done!
+    @list.done!
+    assert_equal(true, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+  end
+  
+  def test_remove_at
+    assert_raises(IndexError) { @list.remove_at(100) }
+    @list.remove_at(0)
+    assert_equal([@todo2, @todo3], @list.to_a)
+  end
+  
+  # def test_to_s
+  #   output = <<-OUTPUT.chomp.gsub /^\s+/, ""
+  #   ---- Today's Todos ----
+  #   [ ] Buy milk
+  #   [ ] Clean room
+  #   [ ] Go to gym
+  #   OUTPUT
+
+  # assert_equal(output, @list.to_s)
+  # end
+  
+  # def test_to_s_2
+  #   @todo1.done!
+  #   output = <<-OUTPUT.chomp.gsub /^\s+/, ""
+  #   ---- Today's Todos ----
+  #   [X] Buy milk
+  #   [ ] Clean room
+  #   [ ] Go to gym
+  #   OUTPUT
+  #   assert_equal(output, @list.to_s)
+  # end
+  
+  # def test_to_s_3
+  # @list.mark_all_done
+  # output = <<-OUTPUT.chomp.gsub /^\s+/, ""
+  # ---- Today's Todos ----
+  # [X] Buy milk
+  # [X] Clean room
+  # [X] Go to gym
+  # OUTPUT
+
+  # assert_equal(output, @list.to_s)
+  # end
+  
+  def test_each
+    done = @list.each { |todo|todo.done! }
+
+    assert_equal(true, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+  end
+
+  def test_each_2
+    done = @list.each { |todo|todo.done! }
+    assert_instance_of(TodoList, done)
+  end
+  
+  def test_select
+    newlist = @list.select {|todo| nil })
+    assert_equal()
   end
 end
